bea8acf60ed7ebfd00f270516678fabc92e0963c
jkreddy123 <reddy.jayakrishna@gmail.com>
2020-12-08 11:21:38 +0000

    inital commit

--- convertAudio.py
+++ convertAudio.py
@@ -1,19 +1,24 @@
 import cherrypy
 import pandas as pd
-import myprocessor
-p = myprocessor.MyProcessor()
+import runmodel
+p = runmodel.runmodel()
 
-class MyWebService(object):
+class convertAudio(object):
 
-   @cherrypy.expose
-   @cherrypy.tools.json_out()
-   @cherrypy.tools.json_in()
-   def process(self):
-      data = cherrypy.request.json
-      df = pd.DataFrame(data)
-      output = p.run(df)
-      return output.to_json()
+  @cherrypy.expose
+  @cherrypy.tools.json_out()
+  @cherrypy.tools.json_in()
+  def index(self):
+      #data = cherrypy.request.json
+      #df = pd.DataFrame(data)
+      model_path = "path/to/model/dir"
+      w2l_bin = "path/to/interactive_streaming_asr_example"
+      path_to_audio_file = '/content/wav2letterInference/numbersAudioMale.wav'
+      output = p.run(model_path,w2l_bin,path_to_audio_file)
+      return output#.to_json()
+  index.exposed = True
+
 if __name__ == '__main__':
-   config = {'server.socket_host': '0.0.0.0'}
+   config = {'server.socket_host': '0.0.0.0','server.socket_port' : int(sys.argv[1])}
    cherrypy.config.update(config)
-   cherrypy.quickstart(MyWebService())
+   cherrypy.quickstart(convertAudio())
--- runmodel.py
+++ runmodel.py
@@ -2,27 +2,27 @@ import os
 import signal
 from subprocess import Popen, PIPE  
 
-
-model_path = "path/to/model/dir"
-w2l_bin = "path/to/interactive_streaming_asr_example"
-w2l_process = Popen(['{} --input_files_base_path={}'.format(w2l_bin, model_path)],
+class runmodel():
+  def run(model_path,w2l_bin,path_to_audio_file):
+ 
+    
+    w2l_process = Popen(['{} --input_files_base_path={}'.format(w2l_bin, model_path)],
                       stdin=PIPE, stdout=PIPE, stderr=PIPE,
                       shell=True)
 
-paths_to_audio_file = ["path/to/audio/file", "path/to/audio/file2", "path/to/audio/file3", ...]
-for path_to_audio_file in paths_to_audio_file:
     # write to the stdin of the process
     # make sure to flush and add \n to the string
     w2l_process.write(b"input={}\n".format(path_to_audio_file))
     w2l_process.stdin.flush()
     
     while True:
-        # read from process stdout
-        output = p.stdout.readline()
-        if output == b'#finish transcribing\n':
-            # finish transcribing an audio
-            break
-        else:
-            print(output)
+      # read from process stdout
+      output = p.stdout.readline()
+      if output == b'#finish transcribing\n':
+        # finish transcribing an audio
+        break
+      else:
+        print(output)
+    return output
 # finish the process
 os.killpg(os.getpgid(w2l_process.pid), signal.SIGTERM)  
