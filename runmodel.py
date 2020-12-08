fc037963146d32a55239c5c17a9f6b972e83f05a
jkreddy123 <reddy.jayakrishna@gmail.com>
2020-12-08 21:59:42 +0530

    Update runmodel.py

--- runmodel.py
+++ runmodel.py
@@ -12,17 +12,18 @@ class runmodel():
 
     # write to the stdin of the process
     # make sure to flush and add \n to the string
-    w2l_process.write(b"input={}\n".format(path_to_audio_file))
+    w2l_process.stdin.write(b"input=%b\n" % path_to_audio_file.encode())
     w2l_process.stdin.flush()
     
     while True:
       # read from process stdout
-      output = p.stdout.readline()
+      output = w2l_process.stdout.readline()
       if output == b'#finish transcribing\n':
         # finish transcribing an audio
         break
       else:
         print(output)
-    os.killpg(os.getpgid(w2l_process.pid), signal.SIGTERM)  
+      
     return output
 # finish the process
+os.killpg(os.getpgid(w2l_process.pid), signal.SIGTERM)
