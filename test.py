d2d11e326eb4acecc37fc24b33f988de741a8bed
jkreddy123 <reddy.jayakrishna@gmail.com>
2020-12-08 06:53:18 +0000

    inital commit

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import cherrypy
+import sys
 
 class HelloWorld:
     def index(self):
         return "Hello World!"
     index.exposed = True
 if __name__ == '__main__':
-   config = {'server.socket_host': '35.190.159.163','server.socket_port' : 8000}
+   config = {'server.socket_host': '0.0.0.0','server.socket_port' : int(sys.argv[1])}
    cherrypy.config.update(config)
    cherrypy.quickstart(HelloWorld())
